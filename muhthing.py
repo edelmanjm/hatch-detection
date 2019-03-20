b5a161300eefa7ee781a4efc8440282300776b91
edelmanjm <edelmanjm@users.noreply.github.com>
2019-03-19 19:00:33 -0700

    Fix bugs with returning vectors
    Matmul in the right order, use R and P during undistortion, better handling for too many centers edge case.

--- muhthing.py
+++ muhthing.py
@@ -35,38 +35,38 @@ class MuhThing:
         self.frame = processed
 
         vectors = np.empty((2, 3), dtype=np.float64)
-        if len(centers) > 0:
+        angles = np.empty((2, 1), dtype=np.float64)
+
+        if len(centers) == 2:
             # Remap to 1xN 2-channel
             # Undistort the points
             if self.scaled_K is not None and self.dist_coefficients is not None:
                 for i, center in enumerate(centers):
                     vectors[i] = np.matmul(
-                        # np.reshape(
-                        #     np.append(
-                        #         np.transpose(
-                        #             cv2.fisheye.undistortPoints(
-                        #                 np.array([[[100, 100]]], dtype=np.float64),
-                        #                 # np.array([[center]], dtype=np.float64),
-                        #                 self.scaled_K,
-                        #                 self.dist_coefficients
-                        #             )[0]
-                        #         ),
-                        #         1
-                        #     ),
-                        #     (1, 3)
-                        # ),
-                        np.array([[400, 400, 1]]),
                         np.linalg.inv(
                             self.scaled_K
-                        )
-                    )[0]
+                        ),
+                        # np.array([320, 240, 1])
+                        np.append(
+                            cv2.fisheye.undistortPoints(
+                                # np.array([[[100, 100]]], dtype=np.float64),
+                                np.array([[center]], dtype=np.float64),
+                                self.scaled_K,
+                                self.dist_coefficients,
+                                R=np.eye(3),
+                                P=self.new_K
+                            )[0][0],
+                            1
+                        ),
+                    )
+
+            for i, vector in enumerate(vectors):
+                angles[i] = np.rad2deg(np.arctan(vector[0] / vector[2]))
+                # angles[i] = np.rad2deg(np.arccos(
+                #     np.clip(np.dot(vector, np.array([0, 0, 1], dtype=np.float64)) / (np.linalg.norm(vector)), -1, 1)))
         else:
             # print("None")
             pass
-
-        angles = np.empty((2, 1), dtype=np.float64)
-        for i, vector in enumerate(vectors):
-            angles[i] = np.rad2deg(np.arccos(np.clip(np.dot(vector, np.array([0, 0, 1], dtype=np.float64)) / (np.linalg.norm(vector)), -1, 1)))
 
         # self.sd.putNumberArray("vectors", [item for sublist in vectors for item in sublist])
         final_value = []
@@ -75,7 +75,7 @@ class MuhThing:
         self.sd.putNumberArray("vectors", final_value)
         NetworkTables.flush()
 
-        print("Found " + str(centers.__len__()) + " targets, latency " + str(round((time.time() - start_time) * 1000)) + "ms: " + str(final_value))
+        print("Found " + str(centers.__len__()) + " targets, latency " + str(round((time.time() - start_time) * 1000)) + "ms: " + str(angles.flatten()))
 
     def start(self):
 
