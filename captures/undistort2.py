19acd22cd47c04dd2f65f0a5604be657eccb854e
edelmanjm <edelmanjm@users.noreply.github.com>
2019-03-27 14:07:22 -0700

    Switch to 160 degree camera

--- captures/undistort2.py
+++ captures/undistort2.py
@@ -5,11 +5,10 @@ import sys
 
 # You should replace these 3 lines with the output in calibration step
 DIM=(2592, 1944)
-K=np.array([[1076.7148792467171, 0.0, 1298.9712963540678], [0.0, 1078.515014983842, 929.9968760065017], [0.0, 0.0, 1.0]])
-D=np.array([[-0.016205134569390902], [-0.02434305021164351], [0.024555436941429715], [-0.008590717479362648]])
+K=np.array([[1368.7651048681387, 0.0, 1209.5888122434037], [0.0, 1370.8987998411494, 951.9292795564025], [0.0, 0.0, 1.0]])
+D=np.array([[-0.11260320941729775], [0.054187476530898164], [-0.04172171303039251], [0.011310942683823726]])
 
-
-def undistort(img_path, balance=1.0, dim2=(2592, 1944), dim3=(2592, 1944)):
+def undistort(img_path, balance=1.0, dim2=(640, 480), dim3=(640, 480)):
     img = cv2.imread(img_path)
     dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
     assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
@@ -24,6 +23,7 @@ def undistort(img_path, balance=1.0, dim2=(2592, 1944), dim3=(2592, 1944)):
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     cv2.imwrite("./" + sys.argv[1] + "_undistorted.jpg", undistorted_img)
+    print(dim3)
     print("scaled_K=np.array(" + str(scaled_K.tolist()) + ")")
     print("new_K=np.array(" + str(new_K.tolist()) + ")")
     # cv2.imshow("undistorted", undistorted_img)
